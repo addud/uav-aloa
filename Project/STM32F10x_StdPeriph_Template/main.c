b7d79b5ac4bba1b384ea5ad487cd5c588412781d
adutze@gmail.com <adutze@gmail.com@4baca7a4-436c-931f-7089-ef5311f548bf>
2012-05-15 15:01:17 +0000

    Data task created. ADC1 infrared sensor data aquisition implemented.
    git-svn-id: https://embedded-uav-quadcopter.googlecode.com/svn/trunk@4 4baca7a4-436c-931f-7089-ef5311f548bf

--- Project/STM32F10x_StdPeriph_Template/main.c
+++ Project/STM32F10x_StdPeriph_Template/main.c
@@ -89,13 +89,17 @@
 #define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
 #endif /* __GNUC__ */
 
+uint16_t readVoltage = 0;
+
 /* Private functions ---------------------------------------------------------*/
 void MyTask(void *pvParameters)
 {
 	int MyCnt=0;
 
-	for(;;)
+	for(;;) {
 		MyCnt++;
+    vTaskDelay(100 / portTICK_RATE_MS);
+  }
 
 }
 
@@ -103,8 +107,90 @@ void AddTask(void *pvParameters)
 {
 	int AddCnt=0;
 
-	for(;;)
+	for(;;) {
 		AddCnt++;
+    vTaskDelay(100 / portTICK_RATE_MS);
+  }
+
+}
+
+void GPIO_Configuration_ADC(void)
+{
+  GPIO_InitTypeDef GPIO_InitStructure;
+
+  /* Configure PC.04 (ADC Channel14) as analog input -------------------------*/
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+}
+
+void RCC_Configuration_ADC(void)
+{
+#if defined (STM32F10X_LD_VL) || defined (STM32F10X_MD_VL) || defined (STM32F10X_HD_VL)
+  /* ADCCLK = PCLK2/2 */
+  RCC_ADCCLKConfig(RCC_PCLK2_Div2); 
+#else
+  /* ADCCLK = PCLK2/4 */
+  RCC_ADCCLKConfig(RCC_PCLK2_Div4); 
+#endif
+  /* Enable peripheral clocks ------------------------------------------------*/
+
+  /* Enable ADC1 and GPIOC clock */
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1 | RCC_APB2Periph_GPIOA, ENABLE);
+}
+
+void DataTask(void *pvParameters)
+{
+  ADC_InitTypeDef ADC_InitStructure;
+  portTickType lastWake = xTaskGetTickCount();
+  
+
+  // init ADC
+  /* System clocks configuration ---------------------------------------------*/
+  RCC_Configuration_ADC();
+
+  /* GPIO configuration ------------------------------------------------------*/
+  GPIO_Configuration_ADC();
+  
+  /* ADC1 configuration ------------------------------------------------------*/
+  ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;
+  ADC_InitStructure.ADC_ScanConvMode = ENABLE;
+  ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;
+  ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
+  ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+  ADC_InitStructure.ADC_NbrOfChannel = 1;
+  ADC_Init(ADC1, &ADC_InitStructure);
+
+  /* ADC1 regular channel14 configuration */ 
+  ADC_RegularChannelConfig(ADC1, ADC_Channel_2, 1, ADC_SampleTime_55Cycles5);
+  
+  /* Enable ADC1 */
+  ADC_Cmd(ADC1, ENABLE);
+
+  /* Enable ADC1 reset calibration register */   
+  ADC_ResetCalibration(ADC1);
+  /* Check the end of ADC1 reset calibration register */
+  while(ADC_GetResetCalibrationStatus(ADC1));
+
+  /* Start ADC1 calibration */
+  ADC_StartCalibration(ADC1);
+  /* Check the end of ADC1 calibration */
+  while(ADC_GetCalibrationStatus(ADC1));
+     
+  /* Start ADC1 Software Conversion */ 
+  ADC_SoftwareStartConvCmd(ADC1, ENABLE);
+
+
+
+
+  for(;;) {
+
+    readVoltage = ADC_GetConversionValue(ADC1);
+
+
+    vTaskDelayUntil(&lastWake, 10 / portTICK_RATE_MS);
+  }
+
 
 }
 
@@ -194,6 +280,8 @@ int main(void)
 
   xTaskCreate( MyTask, ( signed portCHAR * ) "MyTask", configMINIMAL_STACK_SIZE, (void*)NULL, 2, NULL );
   xTaskCreate( AddTask, ( signed portCHAR * ) "AddTask", configMINIMAL_STACK_SIZE, (void*)NULL, 2, NULL );
+
+  xTaskCreate( DataTask, ( signed portCHAR * ) "DataTask", configMINIMAL_STACK_SIZE, (void*)NULL, 2, NULL );
   
   
   /* Start the scheduler. */
